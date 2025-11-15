9e45cfed144438f5a007f65df29f26ebc5366d07
Đặng Hoàng Gia Phú <zafus2103@gmail.com>
2025-11-15 08:44:14 +0700

    chore: add zustand, axios and react-native-paper dependencies

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function Index() {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-          }}
+      }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
